695c0635f6cd1fd5d2cc1edaf7c76bb873ec8a82
Jing <jhu8187@gmail.com>
2023-07-27 18:48:25 -0400

    Add: automate transaction record to google sheet

--- transactions.py
+++ transactions.py
@@ -2,6 +2,7 @@ import gspread
 import csv
 import json
 import os
+from time import sleep
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,6 +13,8 @@ with open('accounts.json') as f:
 
 path = f"{PATH}"
 transactions = []
+
+# Might want to change this to dictionary later, so the desc can be lower case and won't be so long
 markets = ['HMART', 'METRO', 'WHOLE FOODS', 'T&T', 'SHOPPERS']
 foodDelivery = ['DOORDASH', 'EATS']
 transits = ['PRESTO', 'TRIP']
@@ -19,7 +22,7 @@ subscriptions = ['']
 credit_cards = ['Visa', 'MasterCard', 'Amex']
 
 
-def getTransactionData(path, markets=markets, foodDelivery=foodDelivery, transits=transits):
+def getTransactionData(path, credit_cards=credit_cards, markets=markets, foodDelivery=foodDelivery, transits=transits):
     try:
         with open(path, mode='r') as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -32,11 +35,11 @@ def getTransactionData(path, markets=markets, foodDelivery=foodDelivery, transit
                     date = row[2]
                     desc = row[4]
                     amount = row[6]
-                    category = 'Misc'
+                    category = 'Miscellaneous'
                     if row[0] in credit_cards:
                         type = 'Credit Card'
                     if row[1] in accounts[type]:
-                        card_used = accounts[type][str(row[1])]
+                        card_used = accounts[type][row[1]]
                     while res == 1:
                         for market in markets:
                             if market in desc:
@@ -60,21 +63,25 @@ def getTransactionData(path, markets=markets, foodDelivery=foodDelivery, transit
                             category = 'Phone Bill'
                             break
                         if 'PAYMENT' in desc:
-                            category = 'Payment'
+                            category = ''
                             break
                         break
                     res = 1
                     transaction = ((date, desc, category, amount, type, card_used))
-                    print(transaction)
-                    transactions.append(transaction)
+                    if category != '':
+                        transactions.append(transaction)
                 return transactions
             except:
                 print("Fail to extract data from csv file.")
     except:
         print("CSV file cannot be opened.")
 
-# gc = gspread.service_account()
-# sh = gc.open("Transactions sheet")
-# wks = sh.worksheet()
+gc = gspread.service_account()
+sh = gc.open("Transactions sheet")
+wks = sh.worksheet("Sheet1")
 
 rows = getTransactionData(path)
+
+for row in rows:
+    wks.insert_row(row, 2)
+    sleep(2)
