3cfe1a1040fcfda96fae934d85e648b1c6499589
Jing <jhu8187@gmail.com>
2023-08-01 01:01:09 -0400

    Fix variable type for google sheet calculation

--- recordTransactions.py
+++ recordTransactions.py
@@ -8,6 +8,13 @@ import category as cat
 from time import sleep
 import importcsv
 
+# Link to google sheet
+gc = gspread.service_account()
+sh = gc.open("Budget Sheet")
+wks = sh.worksheet("sheet 1")
+
+transactions = []
+
 # Insert data
 with open('accounts.json') as f:
     accounts = json.load(f)
@@ -16,7 +23,6 @@ with open('titles.json') as f:
 key_cats = list(titles.keys())
 with open('keywords.json') as f:
     keyword_list = json.load(f)
-transactions = []
 credit_cards = ('Visa', 'MasterCard', 'Amex')
 NUMOFCAT = 0
 for name in titles[key_cats[0]]:
@@ -48,24 +54,33 @@ def getTransactionDate(file, credit_cards=credit_cards):
                     type = 'other'
                     card_used = ''
                     date = row[2]
-                    desc = row[4]
+                    desc = row[4][:19].lower()
+                    vendor = row[4]
                     amount = row[6]
+                    # Convert negative sign
+                    if (amount.startswith('-')):
+                        amount = amount.replace('-', '')
+                    else:
+                        amount = '-' + amount
                     category = 'Miscellaneous'
+                    rate = ''
                     if row[0] in credit_cards:
                         type = 'Credit Card'
                         if row[1] in accounts[type]:
                             card_used = accounts[type][row[1]]
-                        # Set category and desc
+                            rate = accounts["cash back rate"][card_used][0]
+                        # Set category and vendor
                         for keyword in keyword_dict:
-                            if keyword in desc:
+                            if keyword in vendor:
                                 category = keyword_dict[keyword].getCategory()
-                                desc = keyword_dict[keyword].getVendor()
-                        if 'FEE' in desc:
-                            desc = 'Annual Fee'
+                                vendor = keyword_dict[keyword].getVendor()
+                        if 'annual fee' in desc:
+                            desc = card_used + ' Annual Fee'
+                            vendor = 'Annual Fee'
                             type = 'Annual Fee'
                         # Record transaction
                         if category != 'Payment':
-                            transaction = ((date, desc, category, amount, type, card_used))
+                            transaction = ((desc, vendor, category, float(amount), date, type, card_used, rate, float(amount)*float(rate), 0))
                             print(transaction)
                             transactions.append(transaction)
                 except:
@@ -73,18 +88,13 @@ def getTransactionDate(file, credit_cards=credit_cards):
         return transactions
     except:
         print("Error: Can't locate/open csv file.")
-        
 
-# gc = gspread.service_account()
-# sh = gc.open("Transactions sheet test")
-# wks = sh.worksheet("Sheet1")
+def main():
+    file = loadcsv()
+    entries = getTransactionDate(file)
+    if entries is not None:
+        for entry in entries:
+            wks.insert_row(entry, 27)
+            sleep(2)
 
-# file = loadcsv()
-# rows = getTransactionDate(file)
-# if rows is not None:
-#     for row in rows:
-#         wks.insert_row(row, 2)
-#         sleep(2)
-
-file = loadcsv()
-getTransactionDate(file)
+main()
